492af126e6aa6fdc8205fa37a73fefe9c6fb5cd3
Mhmdomarissa <mhmdomarissa1@gmail.com>
2024-08-06 17:02:11 +0300

    name routing

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,6 +1,6 @@
 <ul class="navabr">
-    <li><a href="/">Home</a></li>
-    <li><a href="/about-me">About</a></li>
-    <li><a href="/contact-me">Contact</a></li>
+    <li><a href="{{route("home")}}">Home</a></li>
+    <li><a href="{{route("about")}}">About</a></li>
+    <li><a href="{{route(contact)}}">Contact</a></li>
 
 </ul>
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages/welcome');
+})->name('home');
 
-Route::get('/about-me', function () {
-    return view('about');
-});
+Route::get('/about-us', function () {
+    return view('pages/about');
+})->name('about');
 
 //(uri)
-Route::view('contact-me',"contact",[
-    'page_name' => 'Contact Me page ',
+Route::view('contact-me',"pages/contact",[
+    'page_name' => 'Contact me page ',
     'page_description' => "<script> this is description</script>"
-]);
+])->name('contact');
 Route::get('category/{id}',function ($id) {
 
     $cats = [
@@ -27,7 +27,7 @@ Route::get('category/{id}',function ($id) {
 
     ];
 
-    return view('category',[
+    return view('pages/category',[
         'the_id' => $cats[$id] ?? "This category does not exist."
     ]) ;
     // return view('category');
